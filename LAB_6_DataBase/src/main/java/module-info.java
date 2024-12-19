4a2298a616ca1c3a556f06d517b100612ea61ab7
Diana Lipkina <fpm.lipkina@bsu.by>
2024-12-19 12:26:15 +0300

    Delete LAB_6_DataBase directory

--- LAB_6_DataBase/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.tabledb {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens org.example.tabledb to javafx.fxml;
-    exports org.example.tabledb;
-}
